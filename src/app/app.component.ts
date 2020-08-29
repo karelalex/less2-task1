d2d56378a1bd3cb828d6ae88bca4d9c9f8dd032b
karelin alex <a.karelin@i-teco.ru>
2020-08-29 17:34:57 +0300

    Добавил радугу

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'less2-task1';
+  colors = {
+    red: 'rgb(255, 0, 0)',
+    orange: 'rgb(255, 127, 0)',
+    yellow: 'rgb(255, 255, 0)',
+    green: 'rgb(0, 255, 0)',
+    blue: 'rgb(0, 0, 255)',
+    indigo: 'rgb(46, 43, 95)',
+    violet: 'rgb(139, 0, 255)'
+  };
+  tableClass = 'normal-table';
+  setStyle = (color): string => `background-color: ${color}`;
+  changeClass = () => {
+    this.tableClass = this.tableClass === 'normal-table' ? 'hidden-table' : 'normal-table';
+  }
+
 }
